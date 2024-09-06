ad125bd110658b3eb187856742be9b918e983154
nada <nada>
2024-09-06 15:11:35 -0400

    UPDATES

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'piano';
+  public title = 'Piano';
+
+  public playSound(sound: string) {
+    console.log(`playing sound: ${sound}`);
+    let audio = new Audio();
+    audio.src = `./src/assets/sounds/${sound}.wav`;
+    audio.load();
+    audio.play();
+  }
 }
